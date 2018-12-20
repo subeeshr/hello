a817c65f58aa2ae7f188099198a7862e8fc3cf2b
subeeshr <mrsubeesh@gmail.com>
2018-12-20 15:49:13 +0000

    dockerbuild.sh updated

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,11 +4,11 @@ set -ex
 
 PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
-IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
+IMAGE_NAME="$PARENT_DIR"_"$CURRENT_DIR"
 TAG="${1}"
-
-REGISTRY="https://789391878624.dkr.ecr.eu-west-1.amazonaws.com"
-
+CMD="aws ecr get-login --no-include-email --region eu-west-1"
+REGISTRY="789391878624.dkr.ecr.eu-west-1.amazonaws.com"
+eval $CMD
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} .
 docker tag ${REGISTRY}/${IMAGE_NAME}:${TAG} ${REGISTRY}/${IMAGE_NAME}:latest
 docker push ${REGISTRY}/${IMAGE_NAME}
