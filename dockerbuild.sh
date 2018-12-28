25cb94b036a20d391a49dee87c5a36a40ce817e2
subeeshr <mrsubeesh@gmail.com>
2018-12-28 11:24:42 +0000

    updated10

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,13 +5,13 @@ set -ex
 export PATH="$PATH:/usr/local/bin"
 export AWS_PROFILE=admin
 cd ~/hello
-PARENT_DIR=$(basename "${PWD%/*}")
-CURRENT_DIR="${PWD##*/}"
-IMAGE_NAME="$PARENT_DIR"_"$CURRENT_DIR"
+#PARENT_DIR=$(basename "${PWD%/*}")
+#CURRENT_DIR="${PWD##*/}"
+IMAGE_NAME=new_repo
+REGISTRY="789391878624.dkr.ecr.eu-west-1.amazonaws.com"
 #TAG="${1}"
 CMD=`aws ecr get-login --no-include-email --region eu-west-1`
-REGISTRY="789391878624.dkr.ecr.eu-west-1.amazonaws.com"
 $CMD
 docker build -t ${REGISTRY}/${IMAGE_NAME}:to_tag .
 docker tag ${REGISTRY}/${IMAGE_NAME}:to_tag ${REGISTRY}/${IMAGE_NAME}:latest
-docker push ${REGISTRY}/${IMAGE_NAME}
+docker push ${REGISTRY}/${IMAGE_NAME}:latest
